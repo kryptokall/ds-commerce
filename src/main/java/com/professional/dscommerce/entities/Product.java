0e6a26e8419db8a2c319dba2950a15a9dc5515c7
kryptokall <lleu.marques29@gmail.com>
2025-02-06 19:39:08 -0300

    OrderItem & OrderItemPK additional methods

--- src/main/java/com/professional/dscommerce/entities/Product.java
+++ src/main/java/com/professional/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.professional.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -82,6 +83,14 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(OrderItem::getOrder).toList();
     }
 
     @Override
