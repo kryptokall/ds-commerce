80e581f218af1ac768f53a2b6adfbe1cc6e142b2
kryptokall <lleu.marques29@gmail.com>
2025-02-14 17:43:36 -0300

    Bean Validation

--- src/main/java/com/professional/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/professional/dscommerce/dto/ProductDTO.java
@@ -1,13 +1,27 @@
 package com.professional.dscommerce.dto;
 
 import com.professional.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     private Long id;
+
+    @Size(min = 3, max = 80, message = "The name must contain between 3 and 80 characters")
+    @NotBlank(message = "Required Field")
     private String name;
+
+    @Size(min = 10, message = "The description must have at least 10 characters")
+    @NotBlank(message = "Required Field")
     private String description;
+
+    @Positive(message = "The price must be a positive value")
     private Double price;
     private String imgUrl;
+
+    public ProductDTO() {
+    }
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -29,19 +43,39 @@ public class ProductDTO {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public Double getPrice() {
         return price;
     }
 
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
     public String getImgUrl() {
         return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 }
